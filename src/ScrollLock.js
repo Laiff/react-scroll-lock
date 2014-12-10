887f8616463c4e664f6cd8d743278ee1b69d7caa
Andrew Laiff <q.laiff@gmail.com>
2014-12-10 16:12:09 +0500

    Add initial version of ScrollLock

---
+++ src/ScrollLock.js
@@ -0,0 +1,78 @@
+/**
+ * Created by laiff on 10.12.14.
+ */
+
+/**
+ * Prevent default behavior for event
+ *
+ * @param e
+ * @returns {boolean}
+ */
+var cancelScrollEvent = function (e) {
+    e.stopImmediatePropagation();
+    e.preventDefault();
+    e.returnValue = false;
+    return false;
+};
+
+var addScrollEventListener = function (elem, handler) {
+    elem.addEventListener('wheel', handler, false);
+};
+
+var removeScrollEventListener = function (elem, handler) {
+    elem.removeEventListener('wheel', handler, false);
+};
+
+/**
+ * @extends ReactCompositeComponentInterface
+ *
+ * @mixin ScrollLock
+ */
+var ScrollLock = {
+
+    componentDidMount: function () {
+        this.scrollLock();
+    },
+
+    componentDidUpdate: function () {
+        this.scrollLock();
+    },
+
+    componentWillUnmount: function () {
+        this.scrollRelease();
+    },
+
+    scrollLock: function () {
+        var elem = this.getDOMNode();
+        if (elem) {
+            addScrollEventListener(elem, this.onScrollHandler);
+        }
+    },
+
+    scrollRelease: function () {
+        var elem = this.getDOMNode();
+        if (elem) {
+            removeScrollEventListener(elem, this.onScrollHandler);
+        }
+    },
+
+    onScrollHandler: function (e) {
+        var elem = this.getDOMNode();
+        var scrollTop = elem.scrollTop;
+        var scrollHeight = elem.scrollHeight;
+        var height = elem.clientHeight;
+        var wheelDelta = e.deltaY;
+        var isDeltaPositive = wheelDelta > 0;
+
+        if (isDeltaPositive && wheelDelta > scrollHeight - height - scrollTop) {
+            elem.scrollTop = scrollHeight;
+            return cancelScrollEvent(e);
+        }
+        else if (!isDeltaPositive && -wheelDelta > scrollTop) {
+            elem.scrollTop = 0;
+            return cancelScrollEvent(e);
+        }
+    }
+};
+
+module.exports = ScrollLock;
